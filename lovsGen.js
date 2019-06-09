e293dd73567be4e5d033a59de6ebef1b9171f8cd
Multi Mink <multimink@notarealemail.com>
2019-06-09 07:50:09 -0400

    Update lovsGen.js

--- lovsGen.js
+++ lovsGen.js
@@ -1,15 +1,24 @@
 var lovs = [
-    'you have the cutest nose, stop FUCKING hating it',
-    'im just testing the button rn and wondering if im gonna delete this entry when im done...i guess not',
-    'morning are bettr when u talk to god frist',
+    'you have the cutest nose...so uk....stop FUCKING hating it ya numpt',
+    'most of the time i think about you when listening to music in the car',
+    'testing the button and wondering if im gonna delete this entry when im done...no',
+    'morning are better when u talk to god frist',
     'damn the generated content is really going downhill at this point',
-    'i literally cannot stop smiling when i see you',
+    'yeahhhh i literally cant stop myself from smiling when i see you',
     'i could be having the shittiest day and one notification from you would make me happy again',
     'before we continue i have to make sure that youre not a \'MAN\' with a \'HOOK\'',
-    'its now 6:24 am and i still cant sleep cause im so excited to show you this terrible project',
-    'dont worry, this will receive more lov updates.....once i fix the responsivity problems',
-    'i actually stopped calling sixth form a waste because if i didnt go i definitely woudlnt have met you',
-    'genuinely dont know why everything you do is cute.'
+    'its now 6:24 am and i cant sleep because im so excited to show you this terrible project',
+    'its now 7:41 am and i still cant sleep because im still excited to show you this terrible project',
+    'im not sleeping because i love you and want to finish this terrible project',
+    'dont worry, this will receive more lov updates.....once i fix all the ui problems',
+    'stopped calling sixth form a waste because it led me to you',
+    'ever just looked at someone and thought "damn i wanna be with them forever"....yeah same!',
+    'kith?',
+    'kith kith kith kith kith',
+    'pets?',
+    'pet pet pet pet pet',
+    'oh damn, forgot that this repository is public....hope nobody finds it'
+
 ]
 
 function moreLovs() 
